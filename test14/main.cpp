f84af7f39d30fa0470252010ae4bc9120e76a346
denghe <denghe@hotmail.com>
2023-07-08 16:07:25 +0800

    writing...

--- test14/main.cpp
+++ test14/main.cpp
@@ -1,113 +1,16 @@
 ﻿// test Task without manager
 #include "main.h"
 
-template<typename R = void>
-struct Task;
-
-namespace detail {
-    template<typename Derived, typename R>
-    struct PromiseBase {
-        struct FinalAwaiter {
-            bool await_ready() const noexcept { return false; }
-            void await_resume() noexcept {}
-            template<typename promise_type>
-            std::coroutine_handle<> await_suspend(std::coroutine_handle<promise_type> curr) noexcept {
-                if (auto &p = curr.promise(); p.prev) return p.prev;
-                else return std::noop_coroutine();
-            }
-        };
-        Task<R> get_return_object() noexcept {
-            auto tmp = Task<R>{ std::coroutine_handle<Derived>::from_promise(*(Derived*)this) };
-            tmp.coro.promise().last = tmp.coro;
-            return tmp;
-        }
-        std::suspend_always initial_suspend() { return {}; }
-        FinalAwaiter final_suspend() noexcept(true) { return {}; }
-        std::suspend_always yield_value(int) { return {}; }
-        void unhandled_exception() { throw; }
-
-        std::coroutine_handle<> prev, last;
-        PromiseBase *root{ this };
-    };
-
-    template<typename R>
-    struct Promise final : PromiseBase<Promise<R>, R> {
-        template<typename T>
-        void return_value(T&& v) { r = std::forward<T>(v); }
-        std::optional<R> r;
-    };
-
-    template<>
-    struct Promise<void> : PromiseBase<Promise<void>, void> {
-        void return_void() noexcept {}
-    };
-}
-
-template<typename R>
-struct [[nodiscard]] Task {
-    using promise_type = detail::Promise<R>;
-    using H = std::coroutine_handle<promise_type>;
-    Task() = delete;
-    Task(H h) { coro = h; }
-    Task(Task const &o) = delete;
-    Task &operator=(Task const &o) = delete;
-    Task(Task &&o) : coro(std::exchange(o.coro, nullptr)) {}
-    Task &operator=(Task &&o) = delete;
-    ~Task() { if (coro) { coro.destroy(); } }
-
-    template<bool needMovePromise>
-    struct Awaiter {
-        bool await_ready() const noexcept { return false; }
-        auto await_suspend(std::coroutine_handle<> prev) noexcept {
-            auto& cp = curr.promise();
-            cp.prev = prev;
-            cp.root = ((H&)prev).promise().root;
-            cp.root->last = curr;
-            return curr;
-        }
-        auto await_resume() -> decltype(auto) {
-            assert(curr.done());
-            auto& p = curr.promise();
-            p.root->last = p.prev;
-            if constexpr (std::is_same_v<void, R>) return;
-            else if constexpr (needMovePromise) return *std::move(p).r;
-            else return *p.r;
-        }
-        H curr;
-    };
-    auto operator co_await() const& noexcept { return Awaiter<false>{coro}; }
-    auto operator co_await() const&& noexcept { return Awaiter<true>{coro}; }
-
-    decltype(auto) operator()() {
-        auto& p = coro.promise();
-        auto& c = p.last;
-        while(c && !c.done()) {
-            c.resume();
-        }
-        if constexpr (!std::is_void_v<R>) return *p.r;
-    }
-    bool IsReady() const { return !coro || coro.done(); }
-    decltype(auto) Result() const { return *coro.promise().r; }
-    void Resume() {
-        auto& c = coro.promise().last;
-        while(c && !c.done()) {
-            c.resume();
-        }
-    }
-    H coro;
-};
-
-
 struct Foo {
     int n = 0;
-    Task<int> get2() {
+    xx::Task<int> get2() {
         co_yield 1;
         co_return 4;    // todo: fix
     }
-    Task<int> get1() {
+    xx::Task<int> get1() {
         co_return co_await get2() * co_await get2();
     }
-    Task<> test() {
+    xx::Task<> test() {
         auto v = co_await get1();
         n += v;
     }
@@ -118,8 +21,7 @@ int main() {
         Foo f;
         auto secs = xx::NowSteadyEpochSeconds();
         for (int i = 0; i < 10000000; ++i) {
-            auto t = f.test();
-            t();
+            f.test().Run<false>();
         }
         std::cout << "foo.n = " << f.n << ", secs = " << xx::NowSteadyEpochSeconds(secs) << "\n";
     }
--- xxlib/xx_task.h
+++ xxlib/xx_task.h
@@ -1,235 +1,94 @@
 ﻿#pragma once
-
 // important: only support static function or lambda !!!  COPY data from arguments !!! do not ref !!!
 
 #include "xx_typetraits.h"
-#include "xx_time.h"
-#include "xx_list.h"
-#include "xx_listlink.h"
-#include "xx_listdoublelink.h"
-
 namespace xx {
-
-    namespace detail {
-        struct TaskBase {
-            std::coroutine_handle<> coro;
-
-            TaskBase() = delete;
-            TaskBase(std::coroutine_handle<> h) {
-                coro = h;
-            }
-            TaskBase(TaskBase const &o) = delete;
-            TaskBase &operator=(TaskBase const &o) = delete;
-            TaskBase(TaskBase &&o) : coro(std::exchange(o.coro, nullptr)) {}
-            TaskBase &operator=(TaskBase &&o) = delete;
-            ~TaskBase() {
-                if (coro) {
-                    coro.destroy();
-                }
-            }
-        };
-
-        template<typename T> concept IsTask = requires(T t) { std::is_base_of_v<TaskBase, T>; };
-
-        struct TaskStore {
-            std::aligned_storage_t<sizeof(TaskBase), sizeof(TaskBase)> store;
-
-            void (*Deleter)(void *){};
-            TaskStore() = default;
-            TaskStore(TaskStore const &) = delete;
-            TaskStore &operator=(TaskStore const &) = delete;
-            TaskStore(TaskStore &&o) : store(o.store), Deleter(std::exchange(o.Deleter, nullptr)) {}
-
-            TaskStore &operator=(TaskStore &&o) {
-                std::swap(store, o.store);
-                std::swap(Deleter, o.Deleter);
-                return *this;
-            }
-
-            ~TaskStore() {
-                if (Deleter) {
-                    Deleter((void *) &store);
-                }
-            }
-
-            template<IsTask T>
-            TaskStore(T &&o) {
-                using U = std::decay_t<T>;
-                Deleter = [](void *p) {
-                    ((U *) p)->~U();
-                };
-                new(&store) U(std::move(o));
-            }
-
-            std::coroutine_handle<> &Coro() {
-                assert(Deleter);
-                return ((TaskBase &) store).coro;
-            }
-        };
-    }
-
     template<typename R = void>
     struct Task;
 
     namespace detail {
+        template<typename Derived, typename R>
         struct PromiseBase {
             struct FinalAwaiter {
                 bool await_ready() const noexcept { return false; }
                 void await_resume() noexcept {}
-
                 template<typename promise_type>
-                std::coroutine_handle<> await_suspend(std::coroutine_handle<promise_type> current) noexcept {
-                    if (auto &p = current.promise(); p.previous) return p.previous;
+                std::coroutine_handle<> await_suspend(std::coroutine_handle<promise_type> curr) noexcept {
+                    if (auto &p = curr.promise(); p.prev) return p.prev;
                     else return std::noop_coroutine();
                 }
             };
-
+            Task<R> get_return_object() noexcept {
+                auto tmp = Task<R>{ std::coroutine_handle<Derived>::from_promise(*(Derived*)this) };
+                tmp.coro.promise().last = tmp.coro;
+                return tmp;
+            }
             std::suspend_always initial_suspend() { return {}; }
             FinalAwaiter final_suspend() noexcept(true) { return {}; }
+            std::suspend_always yield_value(int) { return {}; }
             void unhandled_exception() { throw; }
 
-            std::coroutine_handle<> previous;
+            std::coroutine_handle<> prev, last;
+            PromiseBase *root{ this };
         };
 
         template<typename R>
-        struct Promise final : PromiseBase {
-            static constexpr bool rIsRef = std::is_lvalue_reference_v<R>;
-
-            Task<R> get_return_object() noexcept;
-
-            void return_value(R v) {
-                if constexpr (rIsRef) {
-                    r.reset();
-                    r = v;
-                } else {
-                    r = std::move(v);
-                }
-            }
-
-            std::conditional_t<rIsRef, R, const R &> Result() const &{
-                if constexpr (rIsRef) return r.value();
-                else return r;
-            }
-
-            R &&Result() &&requires(not rIsRef) {
-                return std::move(r);
-            }
-
-        private:
-            std::conditional_t<rIsRef, std::optional<std::reference_wrapper<std::remove_reference_t<R>>>, R> r;
+        struct Promise final : PromiseBase<Promise<R>, R> {
+            template<typename T>
+            void return_value(T&& v) { r = std::forward<T>(v); }
+            std::optional<R> r;
         };
 
         template<>
-        struct Promise<void> : PromiseBase {
-            Task<void> get_return_object() noexcept;
-
+        struct Promise<void> : PromiseBase<Promise<void>, void> {
             void return_void() noexcept {}
         };
     }
 
     template<typename R>
-    struct [[nodiscard]] Task : detail::TaskBase {
-        using detail::TaskBase::TaskBase;
+    struct [[nodiscard]] Task {
         using promise_type = detail::Promise<R>;
         using H = std::coroutine_handle<promise_type>;
+        Task() = delete;
+        Task(H h) { coro = h; }
+        Task(Task const &o) = delete;
+        Task &operator=(Task const &o) = delete;
+        Task(Task &&o) : coro(std::exchange(o.coro, nullptr)) {}
+        Task &operator=(Task &&o) = delete;
+        ~Task() { if (coro) { coro.destroy(); } }
 
-        struct AwaiterBase {
+        struct Awaiter {
             bool await_ready() const noexcept { return false; }
-
-            auto await_suspend(std::coroutine_handle<> previous) noexcept {
-                current.promise().previous = previous;
-                return current;
+            auto await_suspend(std::coroutine_handle<> prev) noexcept {
+                auto& cp = curr.promise();
+                cp.prev = prev;
+                cp.root = ((H&)prev).promise().root;
+                cp.root->last = curr;
+                return curr;
             }
-
-            H current;
-        };
-
-        auto operator
-        co_await() const& noexcept {
-            struct Awaiter : AwaiterBase {
-                auto await_resume() -> decltype(auto) {
-                    if constexpr (std::is_same_v<void, R>) return;
-                    else return this->current.promise().Result();
-                }
-            };
-            return Awaiter{(H &) coro};
-        }
-
-        auto operator
-        co_await() const&& noexcept {
-            struct Awaiter : AwaiterBase {
-                auto await_resume() -> decltype(auto) {
-                    if constexpr (std::is_same_v<void, R>) return;
-                    else return std::move(this->current.promise()).Result();
-                }
-            };
-            return Awaiter{(H &) coro};
-        }
-
-        auto Result() const -> decltype(auto) {
-            return ((H &) coro).promise().Result();
-        }
-    };
-
-    namespace detail {
-        template<typename R>
-        inline Task<R> Promise<R>::get_return_object() noexcept {
-            return Task<R>{std::coroutine_handle<Promise<R>>::from_promise(*this)};
-        }
-
-        inline Task<> Promise<void>::get_return_object() noexcept {
-            return Task<>{std::coroutine_handle<Promise<void>>::from_promise(*this)};
-        }
-    }
-
-    struct TaskManager {
-        xx::ListDoubleLink<detail::TaskStore, int, uint> tasks;
-        std::vector<std::coroutine_handle<>> coros, coros_;
-
-    protected:
-        template<detail::IsTask T>
-        Task<int> RootTask(T t, xx::ListDoubleLinkIndexAndVersion<int, uint> iv) {
-            co_await t;
-            tasks.Remove(iv);
-        };
-    public:
-
-        template<detail::IsTask T>
-        void AddTask(T &&v) {
-            auto c = (typename T::H &) v.coro;
-            c.resume();
-            if (!c.done()) {
-                tasks.Add()(RootTask(std::forward<T>(v), tasks.Tail()));
+            auto await_resume() -> decltype(auto) {
+                assert(curr.done());
+                auto& p = curr.promise();
+                p.root->last = p.prev;
+                if constexpr (std::is_same_v<void, R>) return;
+                else return *p.r;
             }
-        }
-
-        size_t ResumeOnce() {
-            if (coros.empty()) return 0;
-            std::swap(coros, coros_);
-            for (auto &c: coros_) {
-                c();
-            }
-            coros_.clear();
-            return coros.size();
-        }
-
-        struct YieldAwaiter {
-            TaskManager &tm;
-
-            YieldAwaiter(TaskManager &tm) : tm(tm) {}
-            bool await_ready() noexcept { return false; }
-            void await_suspend(std::coroutine_handle<> coro) noexcept {
-                tm.coros.push_back(coro);
-            };
-            void await_resume() noexcept {}
+            H curr;
         };
+        Awaiter operator co_await() const& noexcept { return {coro}; }
 
-        auto Yield() {
-            return YieldAwaiter(*this);
+        template<bool runOnce = true>
+        decltype(auto) Run() {
+            auto& p = coro.promise();
+            auto& c = p.last;
+            while(c && !c.done()) {
+                c.resume();
+                if constexpr(runOnce) return;
+            }
+            if constexpr (!std::is_void_v<R>) return *p.r;
         }
-
-        // todo: Yield( resume conditions )
+        bool IsReady() const { return !coro || coro.done(); }
+        decltype(auto) Result() const { return *coro.promise().r; }
+        H coro;
     };
 }
-
-
