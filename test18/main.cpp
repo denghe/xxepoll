5fae0e452b6a0a50449f6393abb0dde36dab1810
denghe <denghe@hotmail.com>
2023-07-30 12:21:37 +0800

    writing...

---
+++ test18/main.cpp
@@ -0,0 +1,144 @@
+﻿// more task design test
+#include "main.h"
+
+namespace xx {
+    template<typename R = void>
+    struct Task;
+
+    namespace detail {
+        template<typename Derived, typename R>
+        struct PromiseBase {
+            std::coroutine_handle<> prev, last;
+            PromiseBase *root{ this };
+            std::variant<int, void*> y; // any ??? more type support ?
+
+            struct FinalAwaiter {
+                bool await_ready() const noexcept { return false; }
+                void await_resume() noexcept {}
+                template<typename promise_type>
+                std::coroutine_handle<> await_suspend(std::coroutine_handle<promise_type> curr) noexcept {
+                    if (auto &p = curr.promise(); p.prev) return p.prev;
+                    else return std::noop_coroutine();
+                }
+            };
+            Task<R> get_return_object() noexcept {
+                auto tmp = Task<R>{ std::coroutine_handle<Derived>::from_promise(*(Derived*)this) };
+                tmp.coro.promise().last = tmp.coro;
+                return tmp;
+            }
+            template<typename P, class = std::enable_if_t<std::is_pointer_v<P>>>
+            std::suspend_always yield_value(P const& p) {
+                root->y = (void*)p;
+                return {};
+            }
+            std::suspend_always yield_value(int v) {
+                root->y = v;
+                return {};
+            }
+            std::suspend_always initial_suspend() { return {}; }
+            FinalAwaiter final_suspend() noexcept(true) { return {}; }
+            void unhandled_exception() { throw; }
+        };
+
+        template<typename R>
+        struct Promise final : PromiseBase<Promise<R>, R> {
+            std::optional<R> r;
+
+            template<typename T>
+            void return_value(T&& v) { r.emplace(std::forward<T>(v)); }
+        };
+
+        template<>
+        struct Promise<void> : PromiseBase<Promise<void>, void> {
+            void return_void() noexcept {}
+        };
+    }
+
+    template<typename R>
+    struct [[nodiscard]] Task {
+        using promise_type = detail::Promise<R>;
+        using H = std::coroutine_handle<promise_type>;
+        H coro;
+
+        Task() = delete;
+        Task(H h) { coro = h; }
+        Task(Task const &o) = delete;
+        Task &operator=(Task const &o) = delete;
+        Task(Task &&o) : coro(std::exchange(o.coro, nullptr)) {}
+        Task &operator=(Task &&o) {
+            std::swap(coro, o.coro);
+            return *this;
+        }
+        ~Task() { if (coro) { coro.destroy(); } }
+
+        struct Awaiter {
+            bool await_ready() const noexcept { return false; }
+            decltype(auto) await_suspend(std::coroutine_handle<> prev) noexcept {
+                auto& cp = curr.promise();
+                cp.prev = prev;
+                cp.root = ((H&)prev).promise().root;
+                cp.root->last = curr;
+                return curr;
+            }
+            decltype(auto) await_resume() {
+                assert(curr.done());
+                auto& p = curr.promise();
+                p.root->last = p.prev;
+                if constexpr (std::is_same_v<void, R>) return;
+                else return *p.r;
+            }
+            H curr;
+        };
+        Awaiter operator co_await() const& noexcept { return {coro}; }
+
+        decltype(auto) Result() const { return *coro.promise().r; }
+        std::variant<int, void*> const& YieldValue() const { return coro.promise().y; }
+        std::variant<int, void*>& YieldValue() { return coro.promise().y; }
+
+        template<bool runOnce = false>
+        XX_FORCE_INLINE void Run() {
+            auto& p = coro.promise();
+            auto& c = p.last;
+            while(c && !c.done()) {
+                c.resume();
+                if constexpr(runOnce) return;
+            }
+        }
+        operator bool() const { return /*!coro ||*/ coro.done(); }
+        void operator()() { Run<true>(); }
+        bool Resume() { Run<true>(); return *this; }
+    };
+
+    template<typename R>
+    struct IsPod<Task<R>> : std::true_type {};
+}
+
+int main() {
+    auto f = []()->xx::Task<>{
+        std::cout << "a" << std::endl;
+        co_yield 1;
+        std::cout << "b" << std::endl;
+        co_yield 2.1;
+        std::cout << "c" << std::endl;
+        int i = 3;
+        co_yield &i;
+        std::cout << "d" << std::endl;
+        std::cout << "i = " << i << std::endl;
+        co_return;
+    }();
+    while(!f.Resume()) {
+        switch(f.YieldValue().index()) {
+            case 0:
+                std::cout << "y = " << std::get<0>(f.YieldValue()) << std::endl;
+                break;
+            case 1:
+                std::cout << "y = (void*)"<< (size_t)std::get<1>(f.YieldValue()) << std::endl;
+                *(int*)std::get<1>(f.YieldValue()) *= 2;
+                break;
+            default:
+                xx_assert(false);
+        }
+    }
+    std::cout << "end" << std::endl;
+    return 0;
+}
---
+++ test18/main.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#ifndef XXEPOLL_TEST16_MAIN_H_
+#define XXEPOLL_TEST16_MAIN_H_
+
+#include "xx_typetraits.h"
+#include "xx_time.h"
+#include "xx_list.h"
+#include "xx_listlink.h"
+#include <xx_dict.h>
+#include <xx_ptr.h>
+
+#endif
